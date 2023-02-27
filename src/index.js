d31c411cb4368872c4a57f642ceeb7c6b8818569
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2023-02-27 21:10:10 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (!bracketsConfig.length || str.length % 2) return false;
+    const splitedString = str.split('');
+    const stack = [];
+    let nope = true;
+
+    splitedString.forEach(bracket => {
+        const isOpeningBracket = bracketsConfig.some(
+            ([opening, closing]) => opening === bracket,
+        );
+        const isClosingBracket = bracketsConfig.some(
+            ([opening, closing]) => closing === bracket,
+        );
+        if (isOpeningBracket) {
+            const [, closing] = bracketsConfig.find(
+                ([opening, closing]) => opening === bracket,
+            );
+            if (bracket === closing && stack[stack.length - 1] === bracket) {
+                stack.pop();
+            } else {
+                stack.push(bracket);
+            }
+        } else if (isClosingBracket) {
+            const [opening] = bracketsConfig.find(
+                ([opening, closing]) => closing === bracket,
+            );
+            if (stack[stack.length - 1] === opening) {
+                stack.pop();
+            } else {
+                nope = false;
+            }
+        }
+    });
+
+    return stack.length === 0 && nope;
+};
